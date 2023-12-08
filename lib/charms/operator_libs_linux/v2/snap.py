27e7c85f4f97d9eff095c681dc33e6ab8daf2586
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2023-12-08 18:22:29 +0100

    Update lib/charms/operator_libs_linux/v2/snap.py
    Temporary fix copyright header in lib
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/charms/operator_libs_linux/v2/snap.py
+++ lib/charms/operator_libs_linux/v2/snap.py
@@ -1,3 +1,6 @@
+# Copyright 2023 Canonical Ltd.
+# Licensed under the Apache2.0. See LICENSE file in charm source for details.
+
 # Copyright 2021 Canonical Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
