3fbd0afb587512ad58da5755bdfd3a6ac3ac6f56
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2023-12-12 17:17:48 +0100

    bootstrap localhost instead of k8s

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -14,8 +14,7 @@ TESTING_MODEL="$(juju switch)"
 sg microk8s -c "microk8s status --wait-ready"
 # lxd should be installed and inited by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
-# sg microk8s -c "juju bootstrap localhost localhost"
-sg microk8s -c "juju bootstrap microk8s controller"
+sg microk8s -c "juju bootstrap localhost localhost"
 
 echo "Switching to testing model"
 sg microk8s -c "juju switch $TESTING_MODEL"
