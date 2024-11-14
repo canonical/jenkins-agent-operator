6ffe21d66cd9f27e1737f0444b6cc9c6d899720e
Yanks Yoon <yangsoo.yoon@canonical.com>
2024-11-14 03:56:37 +0000

    fix: lib fix for noble sources

--- src/charm.py
+++ src/charm.py
@@ -10,7 +10,6 @@ import logging
 import typing
 
 import ops
-from ops.main import main
 
 import agent_observer
 import service
@@ -118,4 +117,4 @@ class JenkinsAgentCharm(ops.CharmBase):
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main(JenkinsAgentCharm)
+    ops.main(JenkinsAgentCharm)
