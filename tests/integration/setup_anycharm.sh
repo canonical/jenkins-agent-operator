64bec5b068f38060ad8ce408155f9ad451938505
charlie4284 <yangsoo.yoon@canonical.com>
2025-09-25 06:10:33 +0000

    test: install required packages in pre-run script

--- tests/integration/setup_anycharm.sh
+++ tests/integration/setup_anycharm.sh
@@ -3,6 +3,10 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+# Required to install cryptography lib on S390X & PPC
+echo "Installing required packages"
+sudo apt-get install -y cargo pkg-config
+
 echo "Setting up any-charm"
 git clone https://github.com/canonical/any-charm
 
