07d72b8e93abbb26c7d8edbe56fb7b6d7883f337
charlie4284 <yangsoo.yoon@canonical.com>
2025-09-29 15:24:05 +0000

    chore: eject deps setup (image bulider adapted)

--- tests/integration/setup_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copyright 2025 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-# Required to install cryptography lib on S390X & PPC
-echo "Installing required packages"
-sudo apt-get install -y cargo pkg-config
