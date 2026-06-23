3639b9eb5699c22f8b378500d9ae6efb30c2b87d
M2-Farzan <m2_farzan@yahoo.com>
2026-06-23 18:38:27 +0900

    fix: Use recommended args to connect to Jenkins (#161)
    * fix: Use recommended args to connect to Jenkins
    
    * chore: update changelog

--- templates/jenkins_agent.sh
+++ templates/jenkins_agent.sh
@@ -54,7 +54,7 @@ fi
 # Specify the agent as ready
 touch "${JENKINS_HOME}/.ready"
 info "Connecting to jenkins"
-if ! java -jar "${JENKINS_HOME}/agent.jar" -jnlpUrl "${JENKINS_URL}/computer/${JENKINS_AGENT}/slave-agent.jnlp" -workDir "${JENKINS_HOME}" -noReconnect -secret "${JENKINS_TOKEN}"; then
+if ! java -jar "${JENKINS_HOME}/agent.jar" -url "${JENKINS_URL}" -name "${JENKINS_AGENT}" -workDir "${JENKINS_HOME}" -noReconnect -secret "${JENKINS_TOKEN}"; then
     err "Error connecting to jenkins"
     # Remove ready mark if unsuccessful
     rm $JENKINS_HOME/.ready
