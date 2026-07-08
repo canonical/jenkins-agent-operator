69b949a4efb2dd10bd093c0c4329b6a34a483f88
charlie4284 <yangsoo.yoon@canonical.com>
2026-07-08 12:59:15 +0800

    test: fix permissions

--- tests/integration/setup_microk8s.sh
+++ tests/integration/setup_microk8s.sh
@@ -12,20 +12,20 @@
 # Export microk8s config for integration tests
 sudo microk8s config | sudo tee "${GITHUB_WORKSPACE}/kube-config" > /dev/null
 
-sudo sg snap_microk8s -c "microk8s enable storage"
+sudo microk8s enable storage
 # Enable MetalLB for LoadBalancer service support (required by traefik-k8s)
 # Use a local IP range that won't conflict with the host network
-sudo sg snap_microk8s -c "microk8s enable metallb:10.64.140.43-10.64.140.49"
+sudo microk8s enable metallb:10.64.140.43-10.64.140.49
 # Restart metallb - metallb can be flaky and stop setup midway
-sudo sg snap_microk8s -c "microk8s disable metallb"
-sudo sg snap_microk8s -c "microk8s enable metallb:10.64.140.43-10.64.140.49"
-sudo sg snap_microk8s -c "microk8s status --wait-ready"
+sudo microk8s disable metallb
+sudo microk8s enable metallb:10.64.140.43-10.64.140.49
+sudo microk8s status --wait-ready
 # lxd should be installed and inited by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
-sudo sg snap_microk8s -c "juju bootstrap localhost localhost"
+juju bootstrap localhost localhost
 
 echo "bootstrapping secondary microk8s controller"
-sudo sg snap_microk8s -c "juju bootstrap microk8s microk8s"
+juju bootstrap microk8s microk8s
 
 echo "Switching to testing model"
-sudo sg snap_microk8s -c "juju switch localhost"
+juju switch localhost
